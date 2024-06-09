edba537e711a700a8d4dbcfbabde471c3b9b6450
Md. Mostak Shahid <mostak.shahid@gmail.com>
2024-06-10 01:20:42 +0600

    Update class-mos-restricted-categories-for-woocommerce-public.php

--- public/class-mos-restricted-categories-for-woocommerce-public.php
+++ public/class-mos-restricted-categories-for-woocommerce-public.php
@@ -160,5 +160,6 @@ class Mos_Restricted_Categories_For_Woocommerce_Public {
 		}
 		return $is_purchasable;
 	}
+	//https://www.cloudways.com/blog/how-to-remove-hide-or-disable-add-to-cart-button-in-woocommerce/
 
 }
